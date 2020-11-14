b39f467dcc39d4eb94aeeb36c210e2d50027b748
devlog98 <dno1998@hotmail.com>
2020-11-14 12:07:32 -0300

    [ADD] Player Movement

---
+++ Assets/02_Scripts/Player/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Player controller
+ */
+
+namespace devlog98.Player {
+    public class Player : MonoBehaviour {
+        [SerializeField] private PlayerMovement playerMovement; // movement logic
+
+        private void Update() {
+            playerMovement.ExecuteUpdate();
+        }
+
+        private void FixedUpdate() {
+            playerMovement.ExecuteFixedUpdate();
+        }
+    }
+}
---
+++ Assets/02_Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Responsible for Player horizontal movement
+ */
+
+namespace devlog98.Player {
+    [Serializable]
+    public class PlayerMovement : MonoBehaviour {
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private float moveSpeed;
+        private float moveDirection; // left or right (-1 or 1)
+
+        // get input
+        public void ExecuteUpdate() {
+            moveDirection = Input.GetAxisRaw("Horizontal");
+        }
+
+        // apply movement force
+        public void ExecuteFixedUpdate() {
+            rb.AddForce(new Vector2(moveSpeed * moveDirection, rb.velocity.y), ForceMode2D.Force);
+        }
+    }
+}
