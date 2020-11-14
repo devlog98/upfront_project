13159499e1945231bec94f7b6880dbee6560fc5d
devlog98 <dno1998@hotmail.com>
2020-11-14 15:26:11 -0300

    [MOD] Fixing jump collision

--- Assets/02_Scripts/Player/PlayerJump.cs
+++ Assets/02_Scripts/Player/PlayerJump.cs
@@ -11,7 +11,7 @@ namespace devlog98.Player {
         [SerializeField] private float jumpForce;
         [SerializeField] private float gravityForce;
         private float verticalVelocity;
-        private bool canJump;
+        public bool canJump;
 
         [Header("Ground Detection")]
         [SerializeField] private Collider2D collider;
@@ -22,41 +22,48 @@ namespace devlog98.Player {
 
         // initialize variables
         private void Start() {
-            raycastHorizontalOffset = collider.bounds.size.x * 0.5f;
-            raycastVerticalOffset = (collider.bounds.size.y * 0.5f) + 0.05f;
+            raycastHorizontalOffset = collider.bounds.extents.x - 0.03f;
+            raycastVerticalOffset = collider.bounds.extents.y + 0.03f;
         }
 
-        // get input
+        // jump logic
         public void ExecuteUpdate() {
-            // ground check
-            isGrounded = false;
-            for (int i = -1; i <= 1; i++) {
-                Vector2 rayOrigin = new Vector2(transform.position.x + (raycastHorizontalOffset * i), transform.position.y);
-
-                Debug.DrawLine(rayOrigin, (Vector3)rayOrigin - transform.up * raycastVerticalOffset, Color.green);
-                if (Physics2D.Raycast(rayOrigin, -transform.up, raycastVerticalOffset, groundLayer)) {
-                    isGrounded = true;
+            isGrounded = GroundCheck();
+            if (isGrounded) {
+                // reset vertical velocity on ground
+                if (verticalVelocity <= 0) {
                     canJump = true;
                     verticalVelocity = 0;
-                    break;
                 }
+            }
+            else {
+                // fall
+                canJump = false;
+                verticalVelocity -= gravityForce * Time.deltaTime;
             }
 
             // jump
             if (canJump && Input.GetButtonDown("Jump")) {
                 verticalVelocity = jumpForce;
-                canJump = false;
-            }
-
-            // fall
-            if (!isGrounded) {
-                verticalVelocity -= gravityForce * Time.deltaTime;
             }
         }
 
         // apply movement force
         public void ExecuteFixedUpdate() {
             rb.AddForce(new Vector2(rb.velocity.x, verticalVelocity), ForceMode2D.Force);
+        }
+
+        // cast ray to ground
+        private bool GroundCheck() {
+            for (int i = -1; i <= 1; i++) {
+                Vector3 rayOrigin = new Vector2(transform.position.x + (raycastHorizontalOffset * i), transform.position.y);
+                Debug.DrawLine(rayOrigin, new Vector3(rayOrigin.x + (raycastHorizontalOffset * i), rayOrigin.y, rayOrigin.z) + Vector3.down * raycastVerticalOffset, Color.green);
+                if (Physics2D.Raycast(rayOrigin, Vector3.down, raycastVerticalOffset, groundLayer)) {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
