2c3b6f396030a5996c08fb4a1fbd9b88073a38fc
devlog98 <dno1998@hotmail.com>
2020-11-14 21:28:24 -0300

    [ADD] Enemy Movement

---
+++ Assets/02_Scripts/Enemy/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+/*
+ * Enemy controller
+ */
+
+namespace devlog98.Enemy {
+    public class Enemy : MonoBehaviour {
+        [SerializeField] private EnemyMovement enemyMovement; // movement logic
+
+        private void FixedUpdate() {
+            enemyMovement.ExecuteFixedUpdate();
+        }
+    }
+}
---
+++ Assets/02_Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/*
+ * Responsible for Enemy vertical movement
+ */
+
+namespace devlog98.Enemy {
+    public class EnemyMovement : MonoBehaviour {
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private float moveSpeed;
+        private float moveDirection = -1; // always go down
+
+        // apply movement force
+        public void ExecuteFixedUpdate() {
+            rb.velocity = new Vector2(rb.velocity.x, moveSpeed * moveDirection);
+        }
+    }
+}
